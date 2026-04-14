01ed09b575d2cb42efc124d10f928c2ec37fe3e0
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2026-04-14 07:57:36 -0400

    Add files via upload

---
+++ PrimeFinder/src/javaPack/PrimeFinder.java
@@ -0,0 +1,22 @@
+package javaPack;
+
+import java.util.Scanner;
+
+public class PrimeFinder 
+{
+	public static void main(String[] args)
+	{
+		 public int class primesBetween(int lower, int upper);
+		 {
+			 Scanner input = new Scanner(System.in);
+			 System.out.println("Enter lower number");
+				int lownum = input.nextInt();
+				int upnum = 15;
+		 }
+			
+			
+		
+		
+	}
+			
+}		
